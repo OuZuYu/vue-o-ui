1c549237f17e9fb62108a39f19faa76ca4e7382d
joel <oupenguin@gmail.com>
2019-10-17 00:59:35 +0800

    feat: 引入 vue-styleguide

---
+++ styleguide.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  components: 'packages/**/*.vue',
+  exampleMode: 'expand',
+  usageMode: 'expand',
+  sections: [
+    {
+      name: 'OIcon',
+      components: 'packages/icon/src/**/*.vue',
+      content: 'docs/OIcon.md'
+    }
+  ],
+  styleguideDir: 'docs'
+};
