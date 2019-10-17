623e6eedda3bd3864346a8faea5f41663c4d8a80
joel <oupenguin@gmail.com>
2019-10-18 00:50:50 +0800

    chore: styleguide 配置

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,13 +1,8 @@
+const glob = require('glob')
+
 module.exports = {
   components: 'packages/**/*.vue',
   exampleMode: 'expand',
   usageMode: 'expand',
-  sections: [
-    {
-      name: 'OIcon',
-      components: 'packages/icon/src/**/*.vue',
-      content: 'docs/OIcon.md'
-    }
-  ],
-  styleguideDir: 'docs'
+  pagePerSection: true
 };
