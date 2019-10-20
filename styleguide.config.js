3546185425edbaf5bddc5fea4ec9dbfdad7d4df7
joel <oupenguin@gmail.com>
2019-10-21 01:02:46 +0800

    chore: styleguide 配置修改

--- styleguide.config.js
+++ styleguide.config.js
@@ -4,5 +4,9 @@ module.exports = {
   components: 'packages/**/*.vue',
   exampleMode: 'expand',
   usageMode: 'expand',
-  pagePerSection: true
+  pagePerSection: true,
+  ignore: [
+    '**/packages/picker/src/option.vue',
+    '**/packages/steps/src/step.vue'
+  ]
 };
